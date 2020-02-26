b6bd5e05d2ec3b0cf624ad18a8ca1c2fdd8bee49
Nick Mc <nbmc@protonmail.com>
2020-02-26 16:42:06 -0600

    installed packages isomorphic-fetch and react-router-dom.

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
+const App = () => {
+    return (
+        <h1>hi</h1>
+    )
+}
+
+export default App;
