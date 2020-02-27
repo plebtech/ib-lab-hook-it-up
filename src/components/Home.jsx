9f179effaf7b1f164e7200a3bc2f2bdd6f8d4853
Nick Mc <nbmc@protonmail.com>
2020-02-27 12:07:35 -0600

    styling tweaks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
     return (
         <>
-            <h1>welcome.</h1>
-            <h2>routing the studio ghibli api.</h2>
-            <h3>this is a demonstration of routing in react, implementing RESTful api calls to the studio ghibli film catalog.</h3>
-            <h3>use the buttons above to view various sorted data. 'films' returns the studio's body of work, 'people' returns a manifest of characters, and so on. from there you can 'learn more' to focus on a single film/character.</h3>
+            <h1>routing with hooks in react.</h1>
+            <h2>hooks first, routing second.</h2>
+            <h3>this is a demonstration of implementing react hooks in place of class-based components.</h3>
+            <h3>routing is used to access a fake api that provides json data (<a href="https://jsonplaceholder.typicode.com/" target="_new">JSONPlaceholder</a>).</h3>
+            <h3>use the buttons above to view a list of users (sorted by id). from there you can click on a user's name to access more data.</h3>
             <hr />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, possimus.</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto at expedita cum ipsum perspiciatis aperiam voluptatem, provident ipsa laudantium nostrum?</p>
